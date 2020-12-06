a64f611b775ea1f300e098e8f766398825da0e22
FraMuCoder <35667016+FraMuCoder@users.noreply.github.com>
2020-12-06 21:28:27 +0100

    yaml to Arduino code yaml structure improvement

--- tools/yaml2x.py
+++ tools/yaml2x.py
@@ -14,15 +14,21 @@
 import argparse
 import sys
 import os
+import re
 from strictyaml import load, Map, Str, Int, Seq, YAMLError, Optional, Any, Enum, Seq, UniqueSeq
 from jinja2 import Environment, FileSystemLoader
 
+templates_names = {
+  'md'  : 'yaml2md.md.jinja2',
+  'ino' : 'yaml2ino.cpp.jinja2'
+}
 
 schema = Map({"shortName": Str(),
               "longName":  Str(),
               "minVersion":  Int(),
               "maxVersion":  Int(),
               "description": Str(),
+              Optional("baseProtocol") : Str(),
               Optional("participants"): Seq(Map({
                   "name": Str(),
                   Optional("description"): Str()
@@ -31,14 +37,22 @@ schema = Map({"shortName": Str(),
                   "id": Int(),
                   "shortName": Str(),
                   "longName": Str(),
-                  "supportedTypes": UniqueSeq(Enum(['>', '<', '!', '#'])),
                   Optional("sinceVersion"): Int(),
                   Optional("tillVersion"): Int(),
                   "description": Str(),
-                  "structureType":  Enum(['md', 'markdown', 'text']),
-                  "structure": Str()
+                  "structures": Seq(Map({
+                    Optional("senders") : Seq(Str()),
+                    Optional("receivers") : Seq(Str()),
+                    "messageTypes": UniqueSeq(Enum(['>', '<', '!', '#'])),
+                    "description": Str(),
+                    "structureType":  Enum(['md', 'markdown', 'text']),
+                    "structureDesc": Str()
+                  }))
               }))
          })
+
+def verify(yaml):
+  return True
 
 def md_filter(value, deep=1):
 	head = '#' * deep
@@ -53,37 +67,75 @@ def cmd_link(cmd):
   cmd_str = "".join([a.lower() if (a.isalnum()) else '-' if a==' ' else '' for a in cmd_title(cmd)])
   return "#{}".format(cmd_str)
 
-env = Environment(loader=FileSystemLoader(os.path.dirname(__file__)),  ### TODO
+def identifier(s, firstLow=False):
+  strs = re.sub("[^a-zA-Z]", " ", str(s)).split()
+  strs = [s.capitalize() for s in strs]
+  if firstLow:
+    strs[0] = strs[0][0].lower() + strs[0][1:]
+  return "".join(strs)
+
+def upper_identifier(s):
+  return re.sub("[^a-zA-Z]", "_", str(s)).upper()
+
+def comment(s, commentStr="// "):
+  return (commentStr + commentStr.join(s.splitlines(keepends=True))).strip('\n\r')
+
+env = Environment(loader=FileSystemLoader(os.path.dirname(__file__)),
 				  trim_blocks=True,
 				  lstrip_blocks=True)
-env.filters['md'] = md_filter;
-env.filters['cmd_title'] = cmd_title;
-env.filters['cmd_link'] = cmd_link;
+env.filters['md'] = md_filter
+env.filters['cmd_title'] = cmd_title
+env.filters['cmd_link'] = cmd_link
+env.filters['identifier'] = identifier
+env.filters['upper_identifier'] = upper_identifier
+env.filters['comment'] = comment
 
 def read_yaml(input):
     yaml_str = input.read()
-    return load(yaml_str, schema)
 
-def convert_to_md(yaml):
-    global env
-    template = env.get_template('yaml2md.tpl')
-    return template.render(yaml=yaml)
+    try:
+      return load(yaml_str, schema)
+    except YAMLError as error:
+      print(error)
+      return None
+
+def convert(type, context):
+    global env, templates_names
+    templates_name = templates_names[type]
+    template = env.get_template(templates_name)
+    return template.render(context)
 
 
 def main():
     parser = argparse.ArgumentParser(description='Little Less Protocol yaml converter')
-    parser.add_argument('-t', '--type', help='output type, default is markdown (md)', nargs=1, dest='input_type', choices=['md'], default='md')
+    parser.add_argument('-a', '--action', help='action, default is verify (ver)', nargs=1, dest='action', choices=['ver', 'md', 'ino'], default=['ver'])
+    parser.add_argument('-s', '--suffix', help='Protocol suffix, default "A"', nargs='?', dest='suffix', const='', default='A')
+    parser.add_argument('-p', '--participant', help='Protocol participant', nargs=1, dest='participant', default=[''])
     parser.add_argument('-i', help='input file', nargs=1, dest='input', default=[sys.stdin], type=argparse.FileType('r'))
     parser.add_argument('-o', help='output file', nargs=1, dest='output', default=[sys.stdout], type=argparse.FileType('w'))
     args = parser.parse_args()
 
     input = args.input[0]
     output = args.output[0]
+    action = args.action[0]
+    suffix = args.suffix
+    participant = args.participant[0]
 
     yaml = read_yaml(input)
 
-    if args.input_type == 'md':
-        output.write(convert_to_md(yaml))
+    if yaml == None:
+      sys.exit(1)
+
+    if not verify(yaml):
+      sys.exit(1)
+
+    if action != 'ver':
+      context = {
+        'yaml'        : yaml,
+        'suffix'      : suffix,
+        'participant' : participant
+      }
+      output.write(convert(action, context))
 
 if __name__ == '__main__':
     main()
